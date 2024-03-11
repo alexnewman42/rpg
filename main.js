ffab6d1bbe6e565d2f528760ff1a077c7770f446
root <alexnewman42@gmail.com>
2024-03-11 08:55:02 -0400

    Create win location

--- main.js
+++ main.js
@@ -65,7 +65,13 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You die. &#x2620;"
-    }
+    },
+    {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389" 
+    },
 ];
 
 //initialize buttons
